3c04a2f21fb61c1ce96c97a8b759eea40e794fd3
Mr.Green <mrgreens.amazingemail@gmail.com>
2019-01-05 20:00:00 -0500

    code society established

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//"sprites"
 let gameGrid = [["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
   ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
   ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
@@ -20,43 +21,58 @@ let gameGrid = [["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"]
   ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
   ["&lt!","==","==","==","==","==","==","==","==","==","==","!>",],
 ["\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/"]];
+let S = [
+[" .","[]"," ."," ."],
+[" .","[]","[]"," ."],
+[" ."," .","[]"," ."],
+[" ."," ."," ."," ."]
+];
+// INDEPENDANT FUNCTION UNION
 function arrayToString(mapT) {let mapImage = "";for(let row = 0; row < mapT.length; row++) {for(let unit = 0; unit < mapT[row].length; unit++) {mapImage += mapT[row][unit];}mapImage += "<br>";}return mapImage;};
+//VARIABLE DECLARATION DEPOSIT
 let scr = document.querySelector("#scr");
 let grid = document.querySelector("#grid");
 let contr = document.querySelector("#contr");
 let game = document.querySelector("#game");
 let once = true;
 let key;
-function loop() {
-scr.innerHTML = "FULL LINES = <span id=\"lines\">0</span><br>LEVEL = <span id=\"lvl\">0</span><br>SCORE = <span id=\"score\">0</span><br>TIME = <span id=\"time\">0</span>"
+let current;
+let x = 0;
+
+//#####SETUP#####
+function setup() {
+scr.innerHTML = "FULL LINES = <span id=\"lines\">0</span><br>LEVEL = <span id=\"lvl\">0</span><br>SCORE = <span id=\"score\">0</span><br>TIME = <span id=\"time\">0</span><br><br><span id=\"next\"></span>"
 grid.innerHTML = arrayToString(gameGrid);
 contr.innerHTML = "MOVE LEFT: a<br> MOVE RIGHT: b"
 let timer = 0;
 let difficulty = 1000;
 let time = document.querySelector("#time");
 let lvl = document.querySelector("#lvl");
+let next = document.querySelector("#next");
   setInterval(() => {
-    timer++;time.innerHTML = timer+ "s";//TIME
+    timer++;time.innerHTML = timer+ "s";
+  }, difficulty);
+  x = 3;
+};
 
-  /*  if(gameGrid[x + 1][y] !== "[]" && gameGrid[x + 1][y] !== "==") {
-      console.log(gameGrid[x + 1][y]);
-      x++;
-      console.log("A" + x);
-      if(x !== 0) {gameGrid[x - 1][y] = " .";}
-      gameGrid[x][y] = "[]";
-      console.log("B" + x);
-  } else {
-    x = 0;
-    console.log("C" + x);
-}*/
-    grid.innerHTML = arrayToString(gameGrid);
-  }, difficulty);   //end of interval
-}; //end of loop
-//key is pressed key
+//#####DRAW#####
+function draw() {
+  current = S;
+
+
+
+
+  console.log(x);
+  next.innerHTML = arrayToString(current);
+  grid.innerHTML = arrayToString(gameGrid);
+}
+//the thing that makes everything work
 document.addEventListener('keydown', (event) => {
   key = event.key;
   console.log(key);
   if(key === " " && once) {
     once = false;
-    loop();}
-  });
+    setup();}
+
+  draw();
+});
