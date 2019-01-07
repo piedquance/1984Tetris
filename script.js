0322f739a390185fdc615a829d6573a6ae29c396
Mr.Green <mrgreens.amazingemail@gmail.com>
2019-01-07 14:01:03 -0500

    you can rotate

--- script.js
+++ script.js
@@ -21,41 +21,35 @@ let gameGrid = [["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"]
   ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
   ["&lt!","==","==","==","==","==","==","==","==","==","==","!>",],
 ["\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/"]];
-let S = [
+const sprites = [[
 ["","[]",""],
 ["","[]","[]"],
 ["","","[]"],
-];
-let Z = [
+],[
 ["","","[]"],
 ["","[]","[]"],
 ["","[]",""]
-];
-let T = [
+],[
 ["","[]",""],
 ["","[]","[]"],
 ["","[]",""]
-];
-let L = [
+],[
 ["","[]",""],
 ["","[]",""],
 ["","[]","[]"]
-];
-let J = [
+],[
 ["","[]","[]"],
 ["","[]",""],
 ["","[]",""]
-];
-let I = [
+],[
 ["","[]","",""],
 ["","[]","",""],
 ["","[]","",""],
 ["","[]","",""]
-];
-let O = [
+],[
 ["[]","[]"],
 ["[]","[]"]
-];
+]]
 // INDEPENDANT FUNCTION UNION
 function arrayToString(mapT) {let mapImage = "";for(let row = 0; row < mapT.length; row++) {for(let unit = 0; unit < mapT[row].length; unit++) {mapImage += mapT[row][unit];}mapImage += "<br>";}return mapImage;};
 
@@ -105,6 +99,9 @@ function display(grid, sprite, x, y) {
   else return grid;
 };
 
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 //VARIABLE DECLARATION DEPOSIT
 let scr = document.querySelector("#scr");
 let grid = document.querySelector("#grid");
@@ -113,12 +110,15 @@ let game = document.querySelector("#game");
 let once = true;
 let key;
 let current;
+let x = 5;
+let y = 5;
 
 //#####SETUP#####
 function setup() {
 scr.innerHTML = "FULL LINES = <span id=\"lines\">0</span><br>LEVEL = <span id=\"lvl\">0</span><br>SCORE = <span id=\"score\">0</span><br>TIME = <span id=\"time\">0</span><br><br><span id=\"next\"></span>"
 grid.innerHTML = arrayToString(gameGrid);
-contr.innerHTML = "MOVE LEFT: a<br> MOVE RIGHT: d"
+contr.innerHTML = "ROTATE: w"
+current = sprites[getRandomArbitrary(0, 7)]
 let timer = 0;
 let difficulty = 1000;
 let time = document.querySelector("#time");
@@ -131,14 +131,57 @@ let next = document.querySelector("#next");
 
 //#####DRAW#####
 function draw() {
-  current = S;
-  console.log(current);
+let gamePlay = [["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!"," ."," ."," ."," ."," ."," ."," ."," ."," .", " .","!>"],
+  ["&lt!","==","==","==","==","==","==","==","==","==","==","!>",],
+["\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/"]];
 
- gameGrid = display(gameGrid, current, 5, 5);
+
+ switch(key) {
+  /* case "a":
+   if(x !== 0) x--;
+   gamePlay = display(gamePlay, current, x, y);
+    break;
+   case "d":
+   if(x !== 8) x++;
+   gamePlay = display(gamePlay, current, x, y);
+   break;*/
+   case "w" :
+   current = rotateSquare(current);
+   gamePlay = display(gamePlay, current, x, y);
+   break;
+/*   case "s" :
+   if(y !== 17)y++;
+   gamePlay = display(gamePlay, current, x, y);
+   break;*/
+   default :
+   gamePlay = display(gamePlay, current, x, y);
+   break;
+ }
+
+
 
 
   next.innerHTML = arrayToString(current);
-  grid.innerHTML = arrayToString(gameGrid);
+  grid.innerHTML = arrayToString(gamePlay);
 }
 //the thing that makes everything work
 document.addEventListener('keydown', (event) => {
